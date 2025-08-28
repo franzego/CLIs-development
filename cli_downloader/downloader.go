12b7cfabf3ef63e966330c2c31fa353bd007afa3
franzego <davidenenama@gmail.com>
2025-08-28 02:19:24 +0100

    On branch concurrency-feature     Changes to be committed: 	modified:   cli_downloader/.env 	modified:   cli_downloader/downloader.go 	new file:   cli_downloader/psedocode

--- cli_downloader/downloader.go
+++ cli_downloader/downloader.go
@@ -15,77 +15,110 @@ import (
 	"github.com/lpernett/godotenv"
 )
 
-func main() {
-
-	//godotenv for loading environment variables
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error in accessing .env file")
-	}
-	//sending the get request
-	URL_downloader := os.Getenv("Download_url")
-	res, err := http.Get(URL_downloader)
+func GetRequest(url string) (*http.Response, error) {
+	//sending the get request to the outlined url
+	r, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("error with GET request to specified url %v", err)
-		return
-	}
-	defer res.Body.Close()
-	// Check status code
-	if res.StatusCode != http.StatusOK {
-		fmt.Printf("Unexpected status code %v\n", res.StatusCode)
-		return
-	}
+		return nil, fmt.Errorf("error %v with GET request", err)
 
-	//create a directory to store the files
-	dir := os.Getenv("Local_Storage")
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
+	}
+	return r, nil
+
+}
+func CheckStatusCode(r *http.Response) error {
+	// Check status code to see if it is an ok
+	if r.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code %v", r.StatusCode)
+	}
+	return nil
+}
+func CreateDirectory(s string) error {
+	if _, err := os.Stat(s); os.IsNotExist(err) {
 		fmt.Printf("Directory does not exist: %v\n", err)
-		if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+		if err = os.MkdirAll(s, os.ModePerm); err != nil {
 			log.Fatal(err)
 		}
 	}
-
+	return nil
+}
+func SavingFileNAme() (string, error) {
 	// saving the downloaded file with a name which will be imputed by the user
 	filename := bufio.NewReader(os.Stdin)
 	fmt.Println("Save as: ")
 	saveAs, err := filename.ReadString('\n')
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error in writing file name %v\n", err)
-		return
 	}
-	name := strings.TrimSpace(saveAs)
-
+	so := strings.TrimSpace(saveAs)
+	return so, nil
+}
+func MakingFilePAth(n string, so string) string {
 	// making the path that will have the saved file
-	filepath := filepath.Join(dir, name)
-
+	// where n is directory and s is file name
+	filepaths := filepath.Join(n, so)
+	return filepaths
+}
+func CreatingFile(so string) *os.File {
 	// creating the file itself
-	file, err := os.Create(filepath)
+	file, err := os.Create(so)
+	if err != nil {
+		fmt.Printf("Error in creating file: %v", err)
+	}
+	return file
+}
+func main() {
+	//godotenv for loading environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error in accessing .env file")
+	}
+	// load environment variable
+	dir := os.Getenv("Local_Storage")
+	url_downloader := os.Getenv("Download_url")
+	//create a directory to store the files
+	if err := CreateDirectory(dir); err != nil {
+		log.Fatal(err)
+	}
+	//make http get request
+	resp, err := GetRequest(url_downloader)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		defer file.Close()
 	}
+	defer resp.Body.Close()
+	//check status
+	if err := CheckStatusCode(resp); err != nil {
+		log.Fatal(err)
+	}
+	//ask user to save
+	filename, err := SavingFileNAme()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//create filepath
+	file := MakingFilePAth(dir, filename)
+	//show the progress bar
+	f := CreatingFile(file)
+	defer f.Close()
+	//save the file
 	// Handling the progress bar
-	count := 10000
+	count := 1000
 	// create and start new bar
-	bar := pb.StartNew(count)
-
+	//bar := pb.StartNew(count)
+	bar := pb.New64(resp.ContentLength).SetRefreshRate(time.Second).Start()
 	for i := 0; i < count; i++ {
 		bar.Increment()
 		time.Sleep(time.Millisecond)
 	}
-
 	// refresh info every second (default 200ms)
 	bar.SetRefreshRate(time.Second)
 	bar.Set(pb.Bytes, true) // Show as bytes instead of raw numbers
-
 	// Create proxy reader
-	reader := bar.NewProxyReader(res.Body)
-
+	reader := bar.NewProxyReader(resp.Body)
+	// Finish progress bar
+	bar.Finish()
 	//copying the response body(download file itself which is a stream of bytes) into the created file
-	_, err = io.Copy(file, reader)
+	_, err = io.Copy(f, reader)
 	if err != nil {
 		fmt.Printf("error in downloading file %v", err)
 	}
-	// Finish progress bar
-	bar.Finish()
+	fmt.Println("Download Completed")
 }
